0b6bb8b544ea4430c7038c1bfcc29ff3cedb8222
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-26 17:39:59 -0500

    refactor: move logic file to separate folder

---
+++ logic/content.js
@@ -0,0 +1,31 @@
+const getOpponentIds = () => {
+  const playerLinks = document.querySelectorAll(".player.color-icon .user-link");
+  const canGetOpponentId = playerLinks.length === 2;
+  if (canGetOpponentId) {
+    return [playerLinks[0].innerText.trim().toLowerCase().split(" ")[0], playerLinks[1].innerText.trim().toLowerCase().split(" ")[0]];
+  }
+  return null;
+};
+
+const checkOpponentAgainstHistlist = () => (data) => {
+  let opponents = data.opponents || [];
+
+  opponents.forEach((opponent) => {
+    const player1IsOnHislist = player1.endsWith(opponent.toLowerCase());
+    const player2IsOnHislist = player2.endsWith(opponent.toLowerCase());
+
+    if (player1IsOnHislist) chrome.runtime.sendMessage({ type: "notify", opponentId: player1 });
+    if (player2IsOnHislist) chrome.runtime.sendMessage({ type: "notify", opponentId: player2 });
+  });
+};
+
+const checkOpponent = async () => {
+  const opponentIds = getOpponentIds();
+
+  if (opponentIds) {
+    const [player1, player2] = opponentIds;
+    chrome.storage.local.get(["opponents"]);
+  }
+};
+
+window.addEventListener("load", checkOpponent);
